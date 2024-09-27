e9a9666860d7196f3d0ecb3b859abf086ed27bf2
Mezi <emameziebebinwe@gmail.com>
2024-09-27 10:55:19 -0400

    fetch data and update state using useReducer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
+import { useEffect, useReducer } from "react";
 import "./App.css";
 import Header from "./Header";
+import Main from "./Main";
+const initialState = {
+  questions: [],
+  //loading, error,ready,active,finished
+  status: "loading",
+  options: [],
+  correctOption: 0,
+  points: 0,
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, status: "ready", questions: action.payload };
+    case "dataailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Action Unknown");
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    async function getData() {
+      try {
+        const res = await fetch("http://localhost:8000/questions");
+        const data = await res.json();
+        dispatch({ type: "dataReceived", payload: data });
+      } catch (error) {
+        dispatch({ type: "dataFailed" });
+      }
+      // console.log(data);
+    }
+    getData();
+  }, []);
   return (
     <div className="app">
       <Header />
+      <Main />
     </div>
   );
 }
